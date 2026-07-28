073cc4105a7ae602b807072d670924f61972ba76
Ngone060104 <ngonemame67@gmail.com>
2026-07-28 11:41:43 +0000

    correction du fichier ClientRepository pour linker avec la base de donnée

--- src/repositories/ClientRepository.java
+++ src/repositories/ClientRepository.java
@@ -1,7 +1,7 @@
 package repositories;
 
 import entities.Client;
-import config.Database;
+import config.DatabaseConfig;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,11 +9,11 @@ import java.util.List;
 public class ClientRepository {
     
     // La configuration de la base de donnees
-    private Database dbConfig;
+    private DatabaseConfig dbConfig;
 
     // Constructeur : initialise la configuration et cree la table
     public ClientRepository() {
-        this.dbConfig = new Database();
+        this.dbConfig = new DatabaseConfig();
         initTable();
     }
 
